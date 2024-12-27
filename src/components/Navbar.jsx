a8c8c304aca86446703433f398e6e877bb3bc672
Virendra Yadav <virendrayadav.sd@gmail.com>
2024-12-27 21:45:02 +0530

    Light and dark label toggling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = (props) => {
             </div>
             <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
                 <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                    <label className="form-check-label" htmlFor="flexSwitchCheckDefault"> {props.mode === 'light' ? 'Light' : 'Dark'} </label> <br />
+                    <label className="form-check-label" htmlFor="flexSwitchCheckDefault"> {props.mode === 'light' ? 'Dark' : 'Light'} </label> <br />
             </div>
 
         </div>
